00ec35be725ac1479ec0def0a710fd12d19b1982
Antonio Lopes da Cruz <tonycruz1@hotmail.com>
2026-06-06 21:12:57 +0100

    Refactor code structure for improved readability and maintainability

--- Deloitte.TradeDashboard.Infrastructure/Providers/JsonDashboardDataProvider.cs
+++ Deloitte.TradeDashboard.Infrastructure/Providers/JsonDashboardDataProvider.cs
@@ -7,26 +7,31 @@ namespace Deloitte.TradeDashboard.Infrastructure.Providers;
 public class JsonDashboardDataProvider : IDashboardDataProvider
 {
 	public Task<DashboardResponse> GetDashboardAsync(
-		DashboardQuery query,
-		CancellationToken cancellationToken)
+	DashboardQuery query,
+	CancellationToken cancellationToken)
 	{
+		var countries = new List<CountryMetricDto>
+	{
+		new() { Country = "China", Value = 92300 },
+		new() { Country = "India", Value = 61750 },
+		new() { Country = "Turkey", Value = 44120 },
+		new() { Country = "Germany", Value = 28368 },
+		new() { Country = "England", Value = 18500 }
+	};
+
+		if (!string.IsNullOrWhiteSpace(query.Country) &&
+			!query.Country.Equals("All", StringComparison.OrdinalIgnoreCase))
+		{
+			countries = countries
+				.Where(x => x.Country.Equals(query.Country, StringComparison.OrdinalIgnoreCase))
+				.ToList();
+		}
+
 		var response = new DashboardResponse
 		{
-			TotalDeclarations = 245038,
-			TotalGoodsValue = 645345647,
-			TopCountries =
-	   [
-		   new CountryMetricDto
-			{
-				Country = "China",
-				Value = 92300
-			},
-			new CountryMetricDto
-			{
-				Country = "India",
-				Value = 61750
-			}
-	   ]
+			TotalDeclarations = countries.Count == 1 ? (int)countries[0].Value : 245038,
+			TotalGoodsValue = countries.Sum(x => x.Value),
+			TopCountries = countries
 		};
 
 		return Task.FromResult(response);
